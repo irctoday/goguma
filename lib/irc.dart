23b57d73a951ef137dc445d6b9903235c5f54150
Simon Ser <contact@emersion.fr>
2022-02-22 13:31:41 +0100

    irc: add CTCP message parser

--- lib/irc.dart
+++ lib/irc.dart
@@ -482,3 +482,35 @@ class _IRCNameMapEntry<V> {
 
 	_IRCNameMapEntry(this.name, this.value);
 }
+
+/// A CTCP message as defined in:
+/// https://rawgit.com/DanielOaks/irc-rfcs/master/dist/draft-oakley-irc-ctcp-latest.html
+class CtcpMessage {
+	final String cmd;
+	final String? param;
+
+	CtcpMessage(String cmd, [ this.param ]) :
+		this.cmd = cmd.toUpperCase();
+
+	static CtcpMessage? parse(IRCMessage msg) {
+		if (msg.cmd != 'PRIVMSG' && msg.cmd != 'NOTICE') {
+			return null;
+		}
+
+		var s = msg.params[1];
+		if (!s.startsWith('\x01')) {
+			return null;
+		}
+		s = s.substring(1);
+		if (s.endsWith('\x01')) {
+			s = s.substring(0, s.length - 1);
+		}
+
+		var i = s.indexOf(' ');
+		if (i >= 0) {
+			return CtcpMessage(s.substring(0, i), s.substring(i + 1));
+		} else {
+			return CtcpMessage(s);
+		}
+	}
+}
