3b44a021ed6103a7b42d4ce23684eb38423a4fa2
Simon Ser <contact@emersion.fr>
2022-03-01 10:05:06 +0100

    buffer-details-page: linkify topic

--- lib/buffer-details-page.dart
+++ lib/buffer-details-page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import 'client.dart';
 import 'client-controller.dart';
+import 'linkify.dart';
 import 'models.dart';
 
 Widget buildBufferDetailsPage(BuildContext context, BufferModel buf) {
@@ -27,6 +28,26 @@ class BufferDetailsPageState extends State<BufferDetailsPage> {
 	Widget build(BuildContext context) {
 		var buffer = context.watch<BufferModel>();
 		var network = context.watch<NetworkModel>();
+
+		Widget? topic;
+		if (buffer.topic != null) {
+			topic = Column(children: [
+				Container(
+					margin: const EdgeInsets.all(15),
+					// TODO: linkify
+					child: Builder(builder: (context) {
+						var textStyle = DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.2);
+						var linkStyle = textStyle.apply(color: Colors.blue, decoration: TextDecoration.underline);
+						return RichText(
+							textAlign: TextAlign.center,
+							text: linkify(buffer.topic!, textStyle: textStyle, linkStyle: linkStyle),
+						);
+					}),
+				),
+				Divider(),
+			]);
+		}
+
 		return Scaffold(
 			body: CustomScrollView(
 				slivers: [
@@ -41,20 +62,12 @@ class BufferDetailsPageState extends State<BufferDetailsPage> {
 						),
 					),
 					SliverList(delegate: SliverChildListDelegate([
-						if (buffer.topic != null) Column(children: [
-							Container(
-								margin: const EdgeInsets.all(15),
-								child: Builder(builder: (context) => Text(buffer.topic!,
-									textAlign: TextAlign.center,
-									style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.2),
-								)),
-							),
-							Divider(),
-						]),
+						if (topic != null) topic,
 						ListTile(
 							title: Text(network.displayName),
 							leading: Icon(Icons.hub),
 						),
+						// TODO: list of channel members, user details, etc
 					])),
 				],
 			),
