74475d0a3c2739acae830e0ba003cb4e3ae18942
Simon Ser <contact@emersion.fr>
2022-03-25 11:39:52 +0100

    page/join: remove unused import

--- lib/page/join.dart
+++ lib/page/join.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'buffer.dart';
-import '../database.dart';
 import '../irc.dart';
 import '../client.dart';
 import '../client_controller.dart';
