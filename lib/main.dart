4b15412157ea475eef9997009507f6c0088107e8
Simon Ser <contact@emersion.fr>
2022-04-27 21:54:22 +0200

    main: only exit on Error
    Don't exit on other thrown values such as DiagnosticsProperty.

--- lib/main.dart
+++ lib/main.dart
@@ -228,7 +228,7 @@ Future<void> _waitNetworkOnline(NetworkModel network) {
 
 void _handleFlutterError(FlutterErrorDetails details) {
 	FlutterError.dumpErrorToConsole(details, forceReport: true);
-	if (kReleaseMode && !(details.exception is Exception)) {
+	if (kReleaseMode && details.exception is Error) {
 		exit(1);
 	}
 }
