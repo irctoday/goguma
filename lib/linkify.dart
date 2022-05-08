irctoday/goguma

commit 240b29ad1688bd7cafc44ed197d4265b2255d0a0

Migrate to launchUrl
launch is deprecated.

lib/linkify.dart
@@ -11,7 +11,7 @@ TextSpan linkify(String text, { required TextStyle textStyle, required TextStyle
 	return buildTextSpan(
 		elements,
 		onOpen: (link) {
-			launch(link.url);
+			launchUrl(Uri.parse(link.url));
 		},
 		style: textStyle,
 		linkStyle: linkStyle,

lib/page/settings.dart
@@ -140,7 +140,7 @@ class _SettingsPageState extends State<SettingsPage> {
 					title: Text('About'),
 					leading: Icon(Icons.info),
 					onTap: () {
-						launch('https://sr.ht/~emersion/goguma/');
+						launchUrl(Uri.parse('https://sr.ht/~emersion/goguma/'));
 					},
 				),
 				ListTile(

commit 1a98e043d2dd4a6d856a971147508875f5c3c4e3

Always use LaunchMode.externalApplication
For some reason launchUrl defaults to a fullscreen webview on Android.

Fixes: 240b29ad1688 ("Migrate to launchUrl")

lib/linkify.dart
@@ -11,7 +11,7 @@ TextSpan linkify(String text, { required TextStyle textStyle, required TextStyle
 	return buildTextSpan(
 		elements,
 		onOpen: (link) {
-			launchUrl(Uri.parse(link.url));
+			launchUrl(Uri.parse(link.url), mode: LaunchMode.externalApplication);
 		},
 		style: textStyle,
 		linkStyle: linkStyle,

lib/page/settings.dart
@@ -140,7 +140,7 @@ class _SettingsPageState extends State<SettingsPage> {
 					title: Text('About'),
 					leading: Icon(Icons.info),
 					onTap: () {
-						launchUrl(Uri.parse('https://sr.ht/~emersion/goguma/'));
+						launchUrl(Uri.parse('https://sr.ht/~emersion/goguma/'), mode: LaunchMode.externalApplication);
 					},
 				),
 				ListTile(