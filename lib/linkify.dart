532372004f90f9f57186ec93a6b316f4f050ebca
Simon Ser <contact@emersion.fr>
2022-06-23 19:27:13 +0200

    linkify: add support for IRC URLs

--- lib/linkify.dart
+++ lib/linkify.dart
@@ -4,7 +4,11 @@ import 'package:linkify/linkify.dart' as lnk;
 import 'package:url_launcher/url_launcher.dart';
 
 TextSpan linkify(String text, { required TextStyle textStyle, required TextStyle linkStyle }) {
-	var elements = lnk.linkify(text, options: lnk.LinkifyOptions(
+	var linkifiers = const [
+		...lnk.defaultLinkifiers,
+		_IrcUriLinkifier(),
+	];
+	var elements = lnk.linkify(text, linkifiers: linkifiers, options: lnk.LinkifyOptions(
 		humanize: false,
 		defaultToHttps: true,
 	));
@@ -19,4 +23,80 @@ TextSpan linkify(String text, { required TextStyle textStyle, required TextStyle
 		style: textStyle,
 		linkStyle: linkStyle,
 	);
+}
+
+class _IrcUriLinkifier extends Linkifier {
+	const _IrcUriLinkifier();
+
+	@override
+	List<LinkifyElement> parse(List<LinkifyElement> elements, LinkifyOptions options) {
+		var out = <LinkifyElement>[];
+		for (var element in elements) {
+			if (element is TextElement) {
+				_parseText(out, element.text);
+			} else {
+				out.add(element);
+			}
+		}
+		return out;
+	}
+
+	void _parseText(List<LinkifyElement> out, String text) {
+		while (text != '') {
+			var i = text.indexOf('irc://');
+			if (i < 0) {
+				i = text.indexOf('ircs://');
+			}
+			if (i < 0) {
+				out.add(TextElement(text));
+				return;
+			}
+
+			if (i > 0) {
+				out.add(TextElement(text.substring(0, i)));
+				text = text.substring(i);
+			}
+
+			i = 0;
+			for (; i < text.length; i++) {
+				var ch = text[i];
+				if (ch.trim() == '') {
+					break; // whitespace
+				}
+
+				var nextCh = '';
+				if (i + 1 < text.length) {
+					nextCh = text[i + 1];
+				}
+
+				if (nextCh.trim() == '' && _isTrailing(ch)) {
+					break;
+				}
+			}
+
+			out.add(UrlElement(text.substring(0, i)));
+			text = text.substring(i);
+		}
+	}
+
+	// Returns true if the character should be ignored if it's the last one.
+	bool _isTrailing(String ch) {
+		switch (ch) {
+		case '.':
+		case '!':
+		case '?':
+		case ',':
+		case ':':
+		case ';':
+		case ')':
+		case '(':
+		case '[':
+		case ']':
+		case '{':
+		case '}':
+			return true;
+		default:
+			return false;
+		}
+	}
 }
