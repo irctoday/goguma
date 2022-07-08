2e9cdc881a4033f13650c802b9018559100b021b
Simon Ser <contact@emersion.fr>
2022-07-08 12:53:33 +0200

    page/buffer: don't overwrite composer text on swipe
    Prepend the nick, but keep the existing message text if any.

--- lib/page/buffer.dart
+++ lib/page/buffer.dart
@@ -212,8 +212,12 @@ class _BufferPageState extends State<BufferPage> with WidgetsBindingObserver {
 	}
 
 	void _handleMessageSwipe(MessageModel msg) {
-		_composerController.text = '${msg.msg.source!.name}: ';
-		_composerController.selection = TextSelection.collapsed(offset: _composerController.text.length);
+		var prefix = '${msg.msg.source!.name}: ';
+		var text = _composerController.text;
+		if (!_composerController.text.startsWith(prefix)) {
+			_composerController.text = '${msg.msg.source!.name}: ${_composerController.text}';
+			_composerController.selection = TextSelection.collapsed(offset: _composerController.text.length);
+		}
 		_composerFocusNode.requestFocus();
 	}
 
