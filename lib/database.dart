irctoday/goguma

commit 60ddee67aeef9797a318d2a04245b5fb4fa6a211

Load messages in chunks from the DB
Stop loading the full list of messages when opening a buffer. Instead
only load the last 1k. Lazily load the next 1k if the user scrolls up.

Should hopefully fix the delay when tapping a buffer, and should
reduce heap usage.

lib/database.dart
@@ -560,14 +560,21 @@ class DB {
 		};
 	}
 
-	Future<List<MessageEntry>> listMessages(int buffer) async {
+	Future<List<MessageEntry>> listMessagesBefore(int buffer, int? msg, int limit) async {
 		var entries = await _db.rawQuery('''
 			SELECT id, time, buffer, raw
 			FROM Message
-			WHERE buffer = ?
-			ORDER BY time
-		''', [buffer]);
-		return entries.map((m) => MessageEntry.fromMap(m)).toList();
+			WHERE buffer = ? AND (? IS NULL OR id < ?)
+			ORDER BY id DESC LIMIT ?
+		''', [buffer, msg, msg, limit]);
+		var l = entries.map((m) => MessageEntry.fromMap(m)).toList();
+		l.sort((a, b) {
+			if (a.time != b.time) {
+				return a.time.compareTo(b.time);
+			}
+			return a.id!.compareTo(b.id!);
+		});
+		return l;
 	}
 
 	Future<void> storeMessages(List<MessageEntry> entries) async {

lib/models.dart
@@ -483,11 +483,16 @@ class BufferModel extends ChangeNotifier {
 	}
 
 	void populateMessageHistory(List<MessageModel> l) {
-		assert(!messageHistoryLoaded);
-		assert(_messages.isEmpty);
-		// The messages passed here must be already sorted by the caller
-		_messages = l;
-		_messageHistoryLoaded = true;
+		// The messages passed here must be already sorted by the caller, and
+		// must always come before the existing messages
+		if (!_messageHistoryLoaded) {
+			assert(_messages.isEmpty);
+			_messages = l;
+			_messageHistoryLoaded = true;
+		} else if (!l.isEmpty) {
+			assert(l.last.entry.time.compareTo(_messages.first.entry.time) <= 0);
+			_messages = [...l, ..._messages];
+		}
 		notifyListeners();
 	}
 

lib/page/buffer.dart
@@ -90,28 +90,10 @@ class _BufferPageState extends State<BufferPage> with WidgetsBindingObserver {
 		_scrollController.addListener(_handleScroll);
 
 		// Timer.run prevents calling setState() from inside initState()
-		Timer.run(_loadMessages);
-	}
-
-	void _loadMessages() async {
-		var buffer = context.read<BufferModel>();
-		if (!buffer.messageHistoryLoaded) {
-			// TODO: only load a partial view of the messages
-			var entries = await context.read<DB>().listMessages(buffer.id);
-			buffer.populateMessageHistory(entries.map((entry) {
-				return MessageModel(entry: entry);
-			}).toList());
-
-			if (!mounted) {
-				return;
-			}
-
-			if (buffer.messages.length < 100) {
-				_fetchChatHistory();
-			}
-		}
-
-		_updateBufferFocus();
+		Timer.run(() {
+			_fetchChatHistory();
+			_updateBufferFocus();
+		});
 	}
 
 	void _handleMessageSwipe(MessageModel msg) {
@@ -137,11 +119,29 @@ class _BufferPageState extends State<BufferPage> with WidgetsBindingObserver {
 			return;
 		}
 
+		var db = context.read<DB>();
 		var clientProvider = context.read<ClientProvider>();
 		var buffer = context.read<BufferModel>();
 		var client = context.read<Client>();
 
-		if (!buffer.messageHistoryLoaded || !client.caps.enabled.contains('draft/chathistory')) {
+		// First try to load history from the DB, then try from the server
+
+		int? firstMsgId;
+		if (!buffer.messages.isEmpty) {
+			firstMsgId = buffer.messages.first.id;
+		}
+
+		var limit = 1000;
+		var entries = await db.listMessagesBefore(buffer.id, firstMsgId, limit);
+		buffer.populateMessageHistory(entries.map((entry) {
+			return MessageModel(entry: entry);
+		}).toList());
+
+		if (entries.length >= limit) {
+			return;
+		}
+
+		if (!client.caps.enabled.contains('draft/chathistory')) {
 			return;
 		}
 

commit 5027675b754dc1a74d2f18a005b8644308af1c5a

database: fix and optimize DB.listMessagesBefore()
The previous assumption was that IDs of older messages were smaller
than IDs of newer messages. This isn't true in general: scrolling
up will fetch chat history from the server and insert these old
messages with new IDs.

Let's stop using IDs altogether and purely rely on time (like
DB.listMessages() was doing).

As a nice side effect we can also take better advantage of the
index_message_buffer_time index:

    SEARCH Message USING INDEX index_message_buffer_time (buffer=? AND time<?)

ie, we no longer need to put all of the messages for that buffer in
a separate temporary b-tree for sorting.

Fixes: 60ddee67aeef ("Load messages in chunks from the DB")

lib/database.dart
@@ -564,14 +564,14 @@ class DB {
 		var where = 'buffer = ?';
 		var params = [buffer];
 		if (msg != null) {
-			where += ' AND id < ?';
-			params.add(msg);
+			where += ' AND id != ? AND time <= (SELECT time FROM Message WHERE id = ?)';
+			params += [msg, msg];
 		}
 		var entries = await _db.rawQuery('''
 			SELECT id, time, buffer, raw
 			FROM Message
 			WHERE $where
-			ORDER BY id DESC LIMIT ?
+			ORDER BY time DESC LIMIT ?
 		'''
 		, [...params, limit]);
 		var l = entries.map((m) => MessageEntry.fromMap(m)).toList();