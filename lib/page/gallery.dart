6dadd27fa1e76d1a2daf7ae93b14ff0f687710d7
Simon Ser <contact@emersion.fr>
2023-03-01 12:45:28 +0100

    page/gallery: introduce simple gallery

---
+++ lib/page/gallery.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+
+class GalleryPage extends StatefulWidget {
+	static const routeName = '/buffer/gallery';
+
+	final Uri uri;
+
+	const GalleryPage({ Key? key, required this.uri }) : super(key: key);
+
+	@override
+	State<GalleryPage> createState() => _GalleryPageState();
+}
+
+class _GalleryPageState extends State<GalleryPage> {
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				backgroundColor: Colors.black,
+				actions: [
+					IconButton(
+						tooltip: 'Share',
+						icon: const Icon(Icons.share),
+						onPressed: () {
+							Share.share(widget.uri.toString());
+						},
+					),
+				],
+			),
+			backgroundColor: Colors.black,
+			body: Image.network(
+				widget.uri.toString(),
+				filterQuality: FilterQuality.medium,
+				loadingBuilder: (context, child, loadingProgress) {
+					if (loadingProgress == null) {
+						return InteractiveViewer(
+							child: Center(child: child),
+						);
+					}
+					double? progress;
+					if (loadingProgress.expectedTotalBytes != null) {
+						progress = loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!;
+					}
+					return Center(child: CircularProgressIndicator(
+						value: progress,
+						color: Colors.white,
+					));
+				},
+				errorBuilder: (context, error, stackTrace) {
+					return Center(child: Column(
+						mainAxisAlignment: MainAxisAlignment.center,
+						children: [
+							Icon(Icons.error, color: Colors.white),
+							Text(error.toString(), style: TextStyle(color: Colors.white)),
+						],
+					));
+				},
+			),
+		);
+	}
+}
