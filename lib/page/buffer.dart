irctoday/goguma

commit 9e8345c7bd1ee0e8ff4c2185039c34849f677370

page/buffer: show SnackBars above Composer

lib/page/buffer.dart
@@ -432,15 +432,15 @@ class _BufferPageState extends State<BufferPage> with WidgetsBindingObserver {
 					msgList,
 					if (jumpToBottom != null) jumpToBottom,
 				])),
-				Visibility(
-					visible: canSendMessage,
-					maintainState: true,
-					child: Material(elevation: 15, child: Container(
-						padding: EdgeInsets.all(10),
-						child: Composer(key: _composerKey),
-					)),
-				),
 			])),
+			bottomNavigationBar: Visibility(
+				visible: canSendMessage,
+				maintainState: true,
+				child: Material(elevation: 15, child: Container(
+					padding: EdgeInsets.all(10),
+					child: Composer(key: _composerKey),
+				)),
+			),
 		);
 	}
 }

commit a965faa306df755890817ef3b861f1eb32500aea

page/buffer: fix composer hidden when keyboord shows up
Fixes: 9e8345c7bd1e ("page/buffer: show SnackBars above Composer")

lib/page/buffer.dart
@@ -437,10 +437,17 @@ class _BufferPageState extends State<BufferPage> with WidgetsBindingObserver {
 			bottomNavigationBar: Visibility(
 				visible: canSendMessage,
 				maintainState: true,
-				child: Material(elevation: 15, child: Container(
-					padding: EdgeInsets.all(10),
-					child: Composer(key: _composerKey),
-				)),
+				child: Padding(
+					// Hack to keep the bottomNavigationBar displayed when the
+					// virtual keyboard shows up
+					padding: EdgeInsets.only(
+						bottom: MediaQuery.of(context).viewInsets.bottom,
+					),
+					child: Material(elevation: 15, child: Container(
+						padding: EdgeInsets.all(10),
+						child: Composer(key: _composerKey),
+					)),
+				),
 			),
 		);
 	}